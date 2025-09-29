ab8ccd320ad502322b490347ac9a20d33a8f15c5
Md. Shahraj Mashrafe Mashfe <shahrajmashfe@gmail.com>
2025-09-29 16:04:08 +0600

    withdraw functionality on progress

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const validPin = 1234
 
-
+// addmoney feature
 document.getElementById("add-money-btn").addEventListener("click", function (e) {
     e.preventDefault();
     const bank = document.getElementById("bank").value;
@@ -20,4 +20,26 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
     
     const newBalance = availableBalance + amount;
     document.getElementById("available-balance").innerText = newBalance;
+})
+
+// cashout feature
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    const amount = parseInt(document.getElementById("withdraw-amount").value);
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+    const newBalance = availableBalance - amount;
+    document.getElementById("available-balance").innerText = newBalance;
+
+});
+
+
+// toggling features
+document.getElementById("add-button").addEventListener("click", function () {
+    document.getElementById("cashout-parent").style.display = "none";
+    document.getElementById("addmoney-parent").style.display = "block";
+})
+
+document.getElementById("cashout-button").addEventListener("click", function () {
+    document.getElementById("addmoney-parent").style.display = "none";
+    document.getElementById("cashout-parent").style.display = "block"; 
 })
