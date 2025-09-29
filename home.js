71296b1cad8485c268877ad4adeb3028be959db2
Md. Shahraj Mashrafe Mashfe <shahrajmashfe@gmail.com>
2025-09-29 14:10:58 +0600

    Account number and pin validation added

--- home.js
+++ home.js
@@ -1,10 +1,23 @@
-document.getElementById("add-money-btn").addEventListener("click", function(e) {
+const validPin = 1234
+
+
+document.getElementById("add-money-btn").addEventListener("click", function (e) {
     e.preventDefault();
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
     const amount = parseInt(document.getElementById("add-amount").value);
-    const pin = document.getElementById("add-pin").value;
+    const pin = parseInt(document.getElementById("add-pin").value);
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+    if(accountNumber.length < 11){
+        alert("Account number must be 11 digits")
+        return;
+    }
+
+    if(pin !== validPin){
+        alert("Invalid PIN")
+        return;
+    }
+    
     const newBalance = availableBalance + amount;
     document.getElementById("available-balance").innerText = newBalance;
 })
